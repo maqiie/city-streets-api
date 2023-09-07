15da263d3e756b99aebcbd5e853f9263e08fa263
maqiie <candiemp2580@gmail.com>
2023-09-07 21:07:50 +0300

    logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
         rescue_from CanCan::AccessDenied do |exception|
                 redirect_to root_path, alert: "Access denied."
               end
+
+              after_action :set_cors_headers
+              def set_cors_headers
+    response.headers['Access-Control-Expose-Headers'] = 'Authorization'
+  end
 end
